b81c37634d04e7a64a7183eeb55d9b11e46e4965
sathwikhbhat <sathwikhbhat@gmail.com>
2025-01-11 16:21:55 +0530

    Updated Scenes

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -60,7 +60,6 @@ public class Window {
 
         glfwFreeCallbacks(glfwWindow);
         glfwDestroyWindow(glfwWindow);
-
         glfwTerminate();
         glfwSetErrorCallback(null).free();
     }
